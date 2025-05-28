b573256a9cb32f42e333866bc768518f89334c67
ScruffySounds <contactscruffy@gmail.com>
2025-05-27 22:30:45 -0400

    Create htaccess for font, more video collapsible bugfixing

--- videos-script.js
+++ videos-script.js
@@ -1,19 +1,15 @@
-$(document).ready(mainCode)
+//creating collapsibles
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-function mainCode(){
-  //creating collapsibles
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
-  }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function(){
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+          content.style.maxHeight = null;
+      } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+      }
+  });
 }
