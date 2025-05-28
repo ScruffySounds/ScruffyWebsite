52a7e2d9144ca8b50291ec0d8af7bc03690fa0da
ScruffySounds <contactscruffy@gmail.com>
2025-05-27 21:29:47 -0400

    Major update! Add missing content and collapsible video lists.

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(mainCode)
 
 function mainCode(){
+  //creating collapsibles
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+  }
+  
     $(".image-3D").click(openLightbox)
     $(".overlay").click(closeLightbox)
     $(".lightbox-image").click(closeLightbox)
@@ -15,6 +31,9 @@ function mainCode(){
         console.log(coolDex)
         $(".lightbox-image").addClass(coolDex)
         switch(coolDex) {
+            case "OWModel":
+              text.innerHTML="A 3D model of an orrery depicting the solar system from the game Outer Wilds."
+              break;
             case "home":
               text.innerHTML="A study of translucent geometric forms."
               break;
@@ -36,11 +55,20 @@ function mainCode(){
             case "kart":
               text.innerHTML="A 3D model of a toy kart made of lathed wood, which I made for a video about Mario Kart's sound design. Featuring an Arduino-style microchip!"
               break;
+            case "coindy":
+              text.innerHTML="Blue and Red Coins from Super Mario Sunshine reimagined as hard candies."
+              break;
             case "dof":
               text.innerHTML="A stylistic 3D effect in which, as the camera's focal distance changes, 3D models in focus get more polygons, and 3D objects out of focus decimate into fewer polygons."
               break;
             case "bubble":
               text.innerHTML="An exaggerated random color/transparency shader to mimic the thin-film iridescence of soap bubbles."
+              break;
+            case "truchet1":
+            case "truchet2":
+            case "truchet3":
+            case "truchet4":
+              text.innerHTML="Experimentations with Truchet tiles, rotationally asymmetric patterns, using Geometry Nodes in Blender."
               break;
             case "melon":
               text.innerHTML="Two shaders on display. For the watermelon rind: a highly distorted tangential wave texture with splotches of green and yellow. For the watermelon flesh: a volumetric shader that uses vertex paint to shift between red and white. It contains a small random chance of black splotches for seeds."
@@ -56,18 +84,6 @@ function mainCode(){
               break;
             case "crab":
               text.innerHTML="A Peckish Aristocrab from Pikmin 3, blowing bubbles and ensnaring a red Pikmin."
-              break;
-            case "drive":
-              text.innerHTML="A rough digital painting I drew during a cross-country road trip in the summer of 2019."
-              break;
-            case "lady":
-              text.innerHTML="A digital pencil drawing of a lady."
-              break;
-            case "tac":
-              text.innerHTML="A vibrant digital illustration of Tac, an enemy in the Kirby game series, for Ryan Yoshikami's (a.k.a. quarkimo's) TAC series of transcriptions, arrangements, and compositions."
-              break;
-            case "hitbox":
-              text.innerHTML="A promotional poster for a radio show my suitemate and I did at KCIA."
               break;
             case "grant":
               text.innerHTML="A portrait of Grant Sanderson from the YouTube channel 3Blue1Brown, drawn during the time he made math lesson livestreams to watch during the covid-19 pandemic."
@@ -89,6 +105,12 @@ function mainCode(){
               break;
             case "mariowilds":
               text.innerHTML="A scene from Outer Wilds: Echoes of the Eye, in the ring world called the Stranger. Except the protagonist is replaced with Mario, in a Satellaview spacesuit with trusty FLUDD on his back."
+              break;
+            case "nomaizonai":
+              text.innerHTML="A painting of Rauru, the Zonai king from The Legend of Zelda: Tears of the Kingdom, alongside Solanum, the Nomai traveler from Outer Wilds."
+              break;
+            case "ff7":
+              text.innerHTML="Three of the party protagonists from Final Fantasy 7 in a minimalist visual style I've been developing on and off in my videos."
               break;
             case "mario-symphony-1":
               text.innerHTML="Beatburg Fortress, a futuristic neon metropolis of electronic beats."
